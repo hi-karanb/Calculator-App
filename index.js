72d2feac607623d27b57d28fa5d8fa0b251d6a07
Karan <karanprince85@gmail.com>
2024-07-10 07:45:17 +0530

    save

---
+++ index.js
@@ -0,0 +1,40 @@
+var display = document.getElementById("display");
+
+function appendToDisplay(input) {
+  display.value = display.value + input;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
+function square() {
+  try {
+    display.value = Math.pow(eval(display.value), 2);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
+
+function cube() {
+  try {
+    display.value = Math.pow(eval(display.value), 3);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
+
+function squareRoot() {
+  try {
+    display.value = Math.sqrt(eval(display.value));
+  } catch (error) {
+    display.value = "Error";
+  }
+}
